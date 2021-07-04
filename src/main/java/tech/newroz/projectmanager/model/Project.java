736be8acf93f2c7c14b806af723bd64bfb9aaab8
TanvirAhmed1 <tanvirnoor38@yahoo.com>
2021-07-04 11:16:03 +0600

    project model updated

--- src/main/java/tech/newroz/projectmanager/model/Project.java
+++ src/main/java/tech/newroz/projectmanager/model/Project.java
@@ -1,2 +1,62 @@
-package tech.newroz.projectmanager.model;public class Project {
+package tech.newroz.projectmanager.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+public class Project implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String description;
+    private final UUID projectId = UUID.randomUUID();
+    public Project() {
+    }
+
+    public Project(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public UUID getProjectId() {
+        return projectId;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", employeeId=" + projectId +
+                '}';
+    }
 }
