d1f6fd76f4dc8ed3ff28e16a4337d690fa0a49ff
TanvirAhmed1 <tanvirnoor38@yahoo.com>
2021-07-04 11:22:22 +0600

    task model created

---
+++ src/main/java/tech/newroz/projectmanager/model/Task.java
@@ -0,0 +1,52 @@
+package tech.newroz.projectmanager.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+public class Task implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private long id;
+    private String name;
+    private final UUID taskId = UUID.randomUUID();
+
+    public Task() {
+    }
+
+    public Task(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UUID getTaskId() {
+        return taskId;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", taskId=" + taskId +
+                '}';
+    }
+}
