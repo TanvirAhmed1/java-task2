b3e90e43492489060c4e99ba893f9de4a7439b72
TanvirAhmed1 <tanvirnoor38@yahoo.com>
2021-07-04 10:44:37 +0600

    initial commit

---
+++ src/test/java/tech/newroz/projectmanager/ProjectmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.newroz.projectmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
